32bf1075de2b1c4a7467bd12e4af6e6c991c01e7
cgavidiavk <gavidiavk@gmail.com>
2019-03-07 17:39:08 +0100

    pruea

--- app/src/main/java/tmi/busho/MainActivity.java
+++ app/src/main/java/tmi/busho/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //fubhwjedks,.xa<
         setContentView(R.layout.activity_main);
     }
 }
