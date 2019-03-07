74ecfb3604ea3ba0030db393a0e41a8c425fe548
Carlos Gavidia <cgavidia@ucm.es>
2019-03-07 18:38:46 +0100

    Prueba de reconocedor (falla)
    Da fallos de excepcion en el Reconocedor de imágenes

--- app/src/main/java/tmi/busho/ReconocedorCuadros.java
+++ app/src/main/java/tmi/busho/ReconocedorCuadros.java
@@ -1,21 +1,114 @@
 package tmi.busho;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class ReconocedorCuadros extends AppCompatActivity {
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.ColorInfo;
+import com.google.api.services.vision.v1.model.DominantColorsAnnotation;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.ImageProperties;
+import com.google.api.services.vision.v1.model.SafeSearchAnnotation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReconocedorCuadros extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = "ReconocedorCuadros";
+    private static final int RECORD_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyA40SjWGfxwULJNqVjkVvw3rSgWLNTc4m0";
+
+    Button takePicture;
+
+    ProgressBar imageUploadProgress;
 
     ImageView imageView;
-    Bitmap bitmap;
+
+    Spinner spinnerVisionAPI;
+
+    TextView visionAPIData;
+    private Feature feature;
+    private Bitmap bitmap;
+    private String[] visionAPI = new String[]{"LANDMARK_DETECTION", "LOGO_DETECTION", "SAFE_SEARCH_DETECTION", "IMAGE_PROPERTIES", "LABEL_DETECTION"};
+
+    private String api = visionAPI[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reconocedor_cuadros);
+        setContentView(R.layout.activity_main);
+        //ButterKnife.bind(this);
+
+        feature = new Feature();
+        feature.setType(visionAPI[0]);
+        feature.setMaxResults(10);
+
+        spinnerVisionAPI.setOnItemSelectedListener(this);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, visionAPI);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerVisionAPI.setAdapter(dataAdapter);
+
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePictureFromCamera();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePicture.setVisibility(View.VISIBLE);
+        } else {
+            takePicture.setVisibility(View.INVISIBLE);
+            makeRequest(Manifest.permission.CAMERA);
+        }
+    }
+
+    private int checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission);
+    }
+
+    private void makeRequest(String permission) {
+        ActivityCompat.requestPermissions(this, new String[]{permission}, RECORD_REQUEST_CODE);
     }
 
     public void takePictureFromCamera() {
@@ -24,11 +117,166 @@ public class ReconocedorCuadros extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
             bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
             callCloudVision(bitmap, feature);
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == RECORD_REQUEST_CODE) {
+            if (grantResults.length == 0 && grantResults[0] == PackageManager.PERMISSION_DENIED
+                    && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                finish();
+            } else {
+                takePicture.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void callCloudVision(final Bitmap bitmap, final Feature feature) {
+        imageUploadProgress.setVisibility(View.VISIBLE);
+        final List<Feature> featureList = new ArrayList<>();
+        featureList.add(feature);
+
+        final List<AnnotateImageRequest> annotateImageRequests = new ArrayList<>();
+
+        AnnotateImageRequest annotateImageReq = new AnnotateImageRequest();
+        annotateImageReq.setFeatures(featureList);
+        annotateImageReq.setImage(getImageEncodeImage(bitmap));
+        annotateImageRequests.add(annotateImageReq);
+
+
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    VisionRequestInitializer requestInitializer = new VisionRequestInitializer(CLOUD_VISION_API_KEY);
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(requestInitializer);
+
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(annotateImageRequests);
+
+                    Vision.Images.Annotate annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    return convertResponseToString(response);
+                } catch (GoogleJsonResponseException e) {
+                    Log.d(TAG, "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(TAG, "failed to make API request because of other IOException " + e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                visionAPIData.setText(result);
+                imageUploadProgress.setVisibility(View.INVISIBLE);
+            }
+        }.execute();
+    }
+
+    @NonNull
+    private Image getImageEncodeImage(Bitmap bitmap) {
+        Image base64EncodedImage = new Image();
+        // Convert the bitmap to a JPEG
+        // Just in case it's a format that Android understands but Cloud Vision
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        // Base64 encode the JPEG
+        base64EncodedImage.encodeContent(imageBytes);
+        return base64EncodedImage;
+    }
+
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+
+        AnnotateImageResponse imageResponses = response.getResponses().get(0);
+
+        List<EntityAnnotation> entityAnnotations;
+
+        String message = "";
+        switch (api) {
+            case "LANDMARK_DETECTION":
+                entityAnnotations = imageResponses.getLandmarkAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "LOGO_DETECTION":
+                entityAnnotations = imageResponses.getLogoAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "SAFE_SEARCH_DETECTION":
+                SafeSearchAnnotation annotation = imageResponses.getSafeSearchAnnotation();
+                message = getImageAnnotation(annotation);
+                break;
+            case "IMAGE_PROPERTIES":
+                ImageProperties imageProperties = imageResponses.getImagePropertiesAnnotation();
+                message = getImageProperty(imageProperties);
+                break;
+            case "LABEL_DETECTION":
+                entityAnnotations = imageResponses.getLabelAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+        }
+        return message;
+    }
+
+    private String getImageAnnotation(SafeSearchAnnotation annotation) {
+        return String.format("adult: %s\nmedical: %s\nspoofed: %s\nviolence: %s\n",
+                annotation.getAdult(),
+                annotation.getMedical(),
+                annotation.getSpoof(),
+                annotation.getViolence());
+    }
+
+    private String getImageProperty(ImageProperties imageProperties) {
+        String message = "";
+        DominantColorsAnnotation colors = imageProperties.getDominantColors();
+        for (ColorInfo color : colors.getColors()) {
+            message = message + "" + color.getPixelFraction() + " - " + color.getColor().getRed() + " - " + color.getColor().getGreen() + " - " + color.getColor().getBlue();
+            message = message + "\n";
+        }
+        return message;
+    }
+
+    private String formatAnnotation(List<EntityAnnotation> entityAnnotation) {
+        String message = "";
+
+        if (entityAnnotation != null) {
+            for (EntityAnnotation entity : entityAnnotation) {
+                message = message + "    " + entity.getDescription() + " " + entity.getScore();
+                message += "\n";
+            }
+        } else {
+            message = "Nothing Found";
+        }
+        return message;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        api = (String) adapterView.getItemAtPosition(i);
+        feature.setType(api);
+        if (bitmap != null)
+            callCloudVision(bitmap, feature);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
 }
