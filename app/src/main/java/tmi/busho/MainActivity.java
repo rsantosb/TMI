b220a6799a6ffbec27c6819ffcb9b0e710581b21
IvanMont <47639555+IvanMont@users.noreply.github.com>
2019-05-09 20:04:43 +0200

    Actualización del módulo AR

--- app/src/main/java/tmi/busho/MainActivity.java
+++ app/src/main/java/tmi/busho/MainActivity.java
@@ -1,8 +1,8 @@
 package tmi.busho;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
--- app/src/main/java/tmi/busho/MostrarRA.java
+++ app/src/main/java/tmi/busho/MostrarRA.java
@@ -1,9 +1,7 @@
 package tmi.busho;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.ar.core.ArCoreApk;
@@ -13,6 +11,7 @@ import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class MostrarRA extends AppCompatActivity {
 
@@ -20,17 +19,19 @@ public class MostrarRA extends AppCompatActivity {
     private boolean mUserRequestedInstall = true;
 
     Session mSession;
+    private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_r);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
         // Enable AR related functionality on ARCore supported devices only.
-        maybeEnableArButton();
+        //maybeEnableArButton();
     }
 
-    void maybeEnableArButton() {
+    /*void maybeEnableArButton() {
         ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
         if (availability.isTransient()) {
             // Re-query at 5Hz while compatibility is checked in the background.
@@ -51,14 +52,19 @@ public class MostrarRA extends AppCompatActivity {
             //mArButton.setEnabled(false);
             Toast.makeText(getApplicationContext(), "Su dispositivo NO soporta RA", Toast.LENGTH_SHORT).show();
         }
-    }
+    }*/
 
     @Override
     protected void onResume() {
         super.onResume();
 
         // Check camera permission.
-        //
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
         // Make sure ARCore is installed and up to date.
         try {
             if (mSession == null) {
@@ -79,7 +85,20 @@ public class MostrarRA extends AppCompatActivity {
             Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG).show();
             return;
         }
-            return;  // mSession is still null.
+        return;  // mSession is still null.
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
     }
 
 }
